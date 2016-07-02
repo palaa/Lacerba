ef26eb9b9a1e7e5d4951ebcdd54a74d22a6c9db0
Pala <your.email@example.com>
2016-07-02 17:02:34 +0000

    Paperclip setup, cover ed allegati

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -60,7 +60,7 @@ class ContentsController < ApplicationController
     end
 
     def content_params
-      params.require(:content).permit(:titolo, :descrizione, :price)
+      params.require(:content).permit(:titolo, :descrizione, :price, :cover, :allegato)
     end
     
     def check_user
